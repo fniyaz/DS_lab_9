a7bf7a72953746b8c4029170ce3f9bb65e2f96b3
fniyaz <35842362+fniyaz@users.noreply.github.com>
2019-11-02 04:15:48 +0300

    Connected to mongodb

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, redirect, url_for, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+mongo = MongoClient('mongodb://node1:27017,node2:27018,node3:27019/?replicaSet=rs0')
+m_coll = mongo.testdb.messages
 
 
 def generate_messages(messages):
-    return "<br>".join(["{}: {}".format(m[0], m[1]) for m in messages])
+    return "<br>".join(["{}: {}".format(m['name'], m['msg']) for m in messages])
 
 
-msgs = []
 def get_msgs():
-    return msgs
+    return list(m_coll.find())
+
 
 def append_msg(msg):
-    msgs.append(msg)
+    m_coll.insert({'name': msg[0], 'msg': msg[1]})
 
 
 @app.route('/index.html', methods=['POST'])
@@ -27,4 +30,4 @@ def hello():
     return render_template("index.html", messages=generate_messages(msgs))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
